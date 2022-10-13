e6f4607122182bcf13328757931a5e722eedd8db
MochammadYanuarFitroni <moch.yanuar.f@gmail.com>
2022-10-13 19:11:43 +0700

    pop up anjg

---
+++ assets/js/popup.js
@@ -0,0 +1,7 @@
+document.getElementById('button-addData').addEventListener('click', function() {
+    document.querySelector('.popup').style.display = 'flex';
+});
+
+document.getElementById('close-button').addEventListener('click', function() {
+    document.querySelector('.popup').style.display = 'none';
+});
