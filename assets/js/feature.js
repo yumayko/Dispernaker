8f0f4eeef38ee2dd3e762aafb2d2b89834b31178
MochammadYanuarFitroni <moch.yanuar.f@gmail.com>
2022-10-15 13:36:35 +0700

    Update accodion table

--- assets/js/feature.js
+++ assets/js/feature.js
@@ -5,3 +5,10 @@ document.getElementById('button-addData').addEventListener('click', function() {
 document.getElementById('close-button').addEventListener('click', function() {
     document.querySelector('.popup').style.display = 'none';
 });
+
+const according = document.getElementsByClassName('according-container');
+for (let i = 0; i < according.length; i++) {
+    according[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+    });
+}
