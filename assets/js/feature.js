487c7064a078c30b454da64f76238f46f29c122b
MochammadYanuarFitroni <moch.yanuar.f@gmail.com>
2022-10-16 09:20:40 +0700

    update accordion & bug fixing

--- assets/js/feature.js
+++ assets/js/feature.js
@@ -6,9 +6,17 @@ document.getElementById('close-button').addEventListener('click', function() {
     document.querySelector('.popup').style.display = 'none';
 });
 
-const according = document.getElementsByClassName('according-container');
-for (let i = 0; i < according.length; i++) {
-    according[i].addEventListener('click', function() {
-        this.classList.toggle('active');
+const accordionItemHeaders = document.querySelectorAll(".acc-header");
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", event => {
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if(accordionItemHeader.classList.contains("active")) {
+        accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    }
+    else {
+        accordionItemBody.style.maxHeight = 0;
+    }
+    
     });
-}
+});
