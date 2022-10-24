29bccd295449b090745cfb6ad9c5a23a61f2af0f
Haru Touko <ariohartoko@gmail.com>
2022-10-24 22:03:48 +0700

    base api done

--- api/src/domain/repository.go
+++ api/src/domain/repository.go
@@ -3,9 +3,9 @@ package domain
 import "disperinaker-api/model"
 
 type SertRepoAdapter interface {
-	CreateSert(sert model.Sertifikat) (id int, err error)
-	GetSerts() (serts []model.Sertifikat, err error)
-	GetSertByID(id int) (sert model.Sertifikat, err error)
-	UpdateSert(sert model.Sertifikat, id int) error
+	CreateSert(sert model.Sertificate) (id int, err error)
+	GetSerts() (serts []model.Sertificate, err error)
+	GetSertByID(id int) (sert model.Sertificate, err error)
+	UpdateSert(sert model.Sertificate, id int) error
 	DeleteSert(id int) error
 }
