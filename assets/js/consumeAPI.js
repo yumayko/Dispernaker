7d53c070adae2551f37601e8eea0b55aaf785b64
MochammadYanuarFitroni <moch.yanuar.f@gmail.com>
2022-11-07 08:21:56 +0700

    Get Data

--- assets/js/consumeAPI.js
+++ assets/js/consumeAPI.js
@@ -1,3 +1,35 @@
-fetch("http://localhost:8888/health")
+const urlApi = "http://localhost:8888/sertificate/all";
+const tbody = document.getElementById("tbody");
+let data = "";
+const postData = (posts) => {
+    let i = 1;
+    posts.forEach((post) => {
+        data += 
+        `<tr>
+            <td>${i++}</td>
+            <td>${post.name}</td>
+            <td>${post.kecamatan}</td>
+            <td>${post.pelatihan}</td>
+            <td>${post.keterangan}</td>
+            <td><button class="button-edit" id="button-edit">Edit</button></td>
+            <td><button class="button-delete" id="button-delete">Hapus</button></td>
+        </tr>`;
+    })
+    tbody.innerHTML = data;
+};
+
+// get data
+fetch(urlApi)
 .then(response => response.json())
-.then(data => console.log(data));
+.then(data => {
+    const realData = data.data;
+    console.log(realData);
+    postData(realData);
+});
+
+// post data (create data)
+const addForm = document.querySelector('.popup-body');
+addForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('submit');
+});
