756bf01ea6838dc79b33089418c99350d66c2a0e
MochammadYanuarFitroni <moch.yanuar.f@gmail.com>
2022-11-08 22:38:26 +0700

    POST Data

--- assets/js/consumeAPI.js
+++ assets/js/consumeAPI.js
@@ -1,7 +1,7 @@
 const urlApi = "http://localhost:8888/sertificate/all";
 const tbody = document.getElementById("tbody");
 let data = "";
-const postData = (posts) => {
+const GetData = (posts) => {
     let i = 1;
     posts.forEach((post) => {
         data += 
@@ -18,18 +18,41 @@ const postData = (posts) => {
     tbody.innerHTML = data;
 };
 
+const postData = () => {
+    return{
+        name: document.getElementById("nama").value,
+        kecamatan: document.getElementById("kecamatan").value,
+        pelatihan: document.getElementById("pelatihan").value,
+        keterangan: document.getElementById("keterangan").value
+    }
+};
+
 // get data
 fetch(urlApi)
 .then(response => response.json())
 .then(data => {
     const realData = data.data;
     console.log(realData);
-    postData(realData);
+    GetData(realData);
 });
 
 // post data (create data)
 const addForm = document.querySelector('.popup-body');
 addForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log('submit');
+    let payload = postData();
+    fetch("http://localhost:8888/sertificate", {
+        method: 'POST',
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(payload)
+    })
+    .then(response => response.json())
+    .then(data => {
+        const dataArr = [];
+        dataArr.push(data.data);
+        GetData(dataArr);
+        location.reload();
+    })
 });
