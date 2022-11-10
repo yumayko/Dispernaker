812a4a8326f6bbcc13555d8f43a67d3a06997932
MochammadYanuarFitroni <moch.yanuar.f@gmail.com>
2022-11-10 13:59:51 +0700

    delete api (edit masih ngebug)

--- assets/js/consumeAPI.js
+++ assets/js/consumeAPI.js
@@ -1,16 +1,19 @@
-const urlApi = "http://localhost:8888/sertificate/all";
 const tbody = document.getElementById("tbody");
+const Vname = document.getElementById("nama");
+const Vkecamatan = document.getElementById("kecamatan");
+const Vpelatihan = document.getElementById("pelatihan");
+const Vketerangan = document.getElementById("keterangan");
 let data = "";
 const GetData = (posts) => {
     let i = 1;
     posts.forEach((post) => {
         data += 
-        `<tr>
+        `<tr data-id=${post.id}>
             <td>${i++}</td>
-            <td>${post.name}</td>
-            <td>${post.kecamatan}</td>
-            <td>${post.pelatihan}</td>
-            <td>${post.keterangan}</td>
+            <td class="name">${post.name}</td>
+            <td class="kecamatan">${post.kecamatan}</td>
+            <td class="pelatihan">${post.pelatihan}</td>
+            <td class="keterangan">${post.keterangan}</td>
             <td><button class="button-edit" id="button-edit">Edit</button></td>
             <td><button class="button-delete" id="button-delete">Hapus</button></td>
         </tr>`;
@@ -20,18 +23,19 @@ const GetData = (posts) => {
 
 const postData = () => {
     return{
-        name: document.getElementById("nama").value,
-        kecamatan: document.getElementById("kecamatan").value,
-        pelatihan: document.getElementById("pelatihan").value,
-        keterangan: document.getElementById("keterangan").value
+        name: Vname.value,
+        kecamatan: Vkecamatan.value,
+        pelatihan: Vpelatihan.value,
+        keterangan: Vketerangan.value
     }
 };
 
 // get data
-fetch(urlApi)
+fetch("http://localhost:8888/sertificate/all")
 .then(response => response.json())
 .then(data => {
     const realData = data.data;
+    console.log(data);
     console.log(realData);
     GetData(realData);
 });
@@ -54,5 +58,57 @@ addForm.addEventListener('submit', (e) => {
         dataArr.push(data.data);
         GetData(dataArr);
         location.reload();
+    })
+});
+
+// edit & delete data
+const submitEdit = document.querySelector(".submit-button");
+const classData = document.querySelector('.t-body');
+classData.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // delete data
+    let id = e.target.parentElement.parentElement.dataset.id;
+    if(e.target.classList.contains('button-delete')){
+        fetch(`http://localhost:8888/sertificate/${id}`, {
+            method: 'DELETE',
+        })
+        .then(response => response.json())
+        .then(() => location.reload())
+    }
+
+    // edit data
+    if(e.target.classList.contains('button-edit')){
+        document.querySelector('.popup').style.display = 'flex';
+    
+        if(document.querySelector('.popup').style.display === 'flex') {
+            document.querySelector('body').style.overflow = 'hidden';
+            const parent = e.target.parentElement.parentElement;
+            let name = parent.querySelector('.name').textContent;
+            let kecamatan = parent.querySelector('.kecamatan').textContent;
+            let pelatihan = parent.querySelector('.pelatihan').textContent;
+            let keterangan = parent.querySelector('.keterangan').textContent;
+    
+            Vname.value = name;
+            Vkecamatan.value = kecamatan;
+            Vpelatihan.value = pelatihan;
+            Vketerangan.value = keterangan;
+        }
+    }
+    submitEdit.addEventListener('click', () => {
+        fetch(`http://localhost:8888/sertificate/${id}`, {
+            method: 'PUT',
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({
+                name: Vname.value,
+                kecamatan: Vkecamatan.value,
+                pelatihan: Vpelatihan.value,
+                keterangan: Vketerangan.value
+            })
+        })
+        .then(response => response.json())
+        .then(() => location.reload())
     })
 });
